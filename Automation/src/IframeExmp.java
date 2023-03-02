7a7ddd11b7c92f8a924bd136373d30e0f935ac7b
tamishra <tamishra.senapati@qualitestgroup.com>
2023-03-02 15:09:02 +0530

    first commit from GitStuff

--- Automation/src/IframeExmp.java
+++ Automation/src/IframeExmp.java
@@ -4,6 +4,11 @@ public class IframeExmp {
 @Test
 	public void Demo() {
 		System.out.println("Ademo from GitDemo");
+		System.out.println("gccjkkb");
 	}
 
+@Test
+	public void NextDemo() {
+	System.out.println("A demo from GitStuff");
+	}
 }
